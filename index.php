6cbef207ca4df726c3788e6e811290bb849088a9
Bruno Geronimo <contato@brunogeronimo.com.br>
2017-03-20 18:07:32 -0300

    Fontes e links corrigidos

--- index.php
+++ index.php
@@ -33,53 +33,66 @@
 
         <div class="container">
 
-            <div class="media" style="width: 70%;">
+            <div class="media">
               <div class="media-left">
-                <img class="media-object" src="/images/64x64.jpg" width="150px">
+                <a href="/pesquisa-de-precos-no-varejo.php">
+                  <img class="media-object" src="/images/64x64.jpg" width="150px">
+                </a>
               </div>
               <div class="media-body">
-                <h3 class="media-heading" style="color:#0274a6"><b>
-                    Pesquisa de preços no varejo</b>
+                <h3 class="media-heading">
+                  <a href="/pesquisa-de-precos-no-varejo.php"><b>Pesquisa de preços no varejo</b></a>
                 </h3>
                 <p align="justify">Se você tem perdido vendas para o seu concorrente e recebido várias reclamações de clientes usuais sobre os seus preços, talvez você encontre neste post a solução para seus problemas.</p>
                 <a href="/pesquisa-de-precos-no-varejo.php">CONTINUE LENDO...</a>
               </div>
             </div>
 
-            <div class="media" style="width: 70%;">
+            <div class="media">
               <div class="media-left">
-                <img class="media-object" src="/images/64x64.jpg" width="150px">
+                <a href="/saque-em-contas-inativas-do-fgts-e-sua-influencia-no-varejo.php">
+                  <img class="media-object" src="/images/64x64.jpg" width="150px">
+                </a>
               </div>
               <div class="media-body">
-                <h3 class="media-heading" style="color:#0274a6"><b>
-                    Saque em contas inativas do FGTS e sua influência no varejo</b>
-
+                <h3 class="media-heading">
+                  <a href="/saque-em-contas-inativas-do-fgts-e-sua-influencia-no-varejo.php">
+                    <b>Saque em contas inativas do FGTS e sua influência no varejo</b>
+                  </a>
                 </h3>
                 <p align="justify">No mês de dezembro de 2016 o presidente Michel Temer anunciou que liberaria o saque de contas inativas do FGTS até dezembro de 2015. Essa medida é parte de um plano do governo para reaquecer a economia através de uma recomposição de renda para os trabalhadores. </p>
                 <a href="/saque-em-contas-inativas-do-fgts-e-sua-influencia-no-varejo.php">CONTINUE LENDO...</a>
               </div>
             </div>
 
-            <div class="media" style="width: 70%;">
+            <div class="media">
               <div class="media-left">
-                <img class="media-object" src="/images/64x64.jpg" width="150px">
+                <a href="/a-chave-do-sucesso-no-varejo-online-e-offline.php">
+                  <img class="media-object" src="/images/64x64.jpg" width="150px">
+                </a>
               </div>
               <div class="media-body">
-                <h3 class="media-heading" style="color:#0274a6"><b>
-                   A chave para o sucesso no varejo</b>
+                <h3 class="media-heading">
+                  <a href="/a-chave-do-sucesso-no-varejo-online-e-offline.php">
+                    <b>A chave para o sucesso no varejo</b>
+                  </a>
                 </h3>
                 <p align="justify">Atualmente as decisões de compra da maioria dos consumidores dependem fortemente do ambiente em que a venda acontece, por isso, faz sentido que os varejistas estejam procurando maneiras mais inovadoras de envolver os clientes em todos os pontos de contato, tanto online quanto na loja física.</p>
                 <a href="">CONTINUE LENDO...</a>
               </div>
             </div>
 
-            <div class="media" style="width: 70%;">
+            <div class="media">
               <div class="media-left">
-                <img class="media-object" src="/images/64x64.jpg" width="150px">
+                <a href="/o-que-esperar-do-varejo-em-2017.php">
+                  <img class="media-object" src="/images/64x64.jpg" width="150px">
+                </a>
               </div>
               <div class="media-body">
-                <h3 class="media-heading" style="color:#0274a6"><b>
-                    O que esperar do Varejo em 2017</b>
+                <h3 class="media-heading">
+                  <a href="/o-que-esperar-do-varejo-em-2017.php">
+                    <b>O que esperar do Varejo em 2017</b>
+                  </a>
                 </h3>
                 <p align="justify">Se tem algo que está em evidência no mundo, é a mudança do consumidor. O público tem amadurecido seu modelo de compras a cada dia, tornando-se mais exigentes, procurando algo que vá além do lugar-comum. Sendo assim, o mundo do varejo também enfrenta uma nova realidade, de compreender esse novo target para que possa atendê-lo de acordo com seus anseios.</p>
                 <a href="/o-que-esperar-do-varejo-em-2017.php">CONTINUE LENDO...</a>

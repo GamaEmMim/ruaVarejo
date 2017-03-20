33ef5a28085725a9f11197d82a818ce4ff6f5e45
JC Bombardelli <jc.bombardelli@live.com>
2017-03-20 19:26:52 -0300

    Pagina de SOBRE

--- sobre.php
+++ sobre.php
@@ -13,11 +13,39 @@
   <body>
     <?php require(__DIR__."/components/navigator.html"); ?>
     <?php require(__DIR__."/components/page_header.html");?>
-    <div class="container">
-      <h1>SOBRE NÓS</h1>
-      <p>LOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMVV</p>
+    
 
-      <p>LOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUM</p>
+     <div class="col-md-9" style="padding-right:10%; padding-left: 10%; padding-bottom: 5%">
+        <h1><p align="justify">SOBRE A RUA VAREJO</h1></p>
+        <h2 style="color:#808080">E como podemos te ajudar com o mercado de varejo.</h2>
+        
+
+      <div class="clearfix colelem" id="u12639-32"><!-- content -->
+       <h3><p align="justify">O Rua Varejo é um blog criado em 2017 com o objetivo de disseminar conhecimento sobre o mercado de varejo. Com foco em novidades e tendências, o blog deseja enriquecer os profissionais e empresas atuantes nesse setor com informações relevantes que possam trazer novas ações e sucesso para os leitores.</p></h3>
+
+
+       <h3><p align="justify">Para que esse conteúdo agregue conhecimento aos leitores, buscamos informações em fontes confiáveis e cases reais para exemplificar as tendências do varejo acontecendo na prática</p></h3>
+       <h6>&nbsp;</h6>
+
+
+
+       <h2><p align="justify">Você também pode nos encontrar nas redes sociais através do nosso  
+         <a href="https://www.linkedin.com/company/16219656" target="_blank"> Linkedin </a> e da nossa página do 
+         <a href="http://www.facebook.com/ruavarejo" target="_blank"> Facebook </a> e verifique nossas outros posts...</p></h2>
+         <h6>&nbsp;</h6>
+
+      </div>
+
+
+
+    </div>
+
+    <div class="col-md-2">
+      <a class="signUpModal" href="#">
+        <div class="row">
+          <img src="/images/banner.jpg">
+        </div>
+      </a>
     </div>
   </body>
 </html>

a9cd71ecbe88d7188134e5ebd01b2d8fb61c3487
Hiagor Queiros de Oliveira <hiagornew@gmail.com>
2017-03-21 01:54:57 -0300

    Formulario de pesquisa em criação

--- pesquisa.php
+++ pesquisa.php
@@ -22,20 +22,62 @@
       <div class="clearfix colelem" id="u12639-32"><!-- content -->
 
         <div style="width: 50%">
-          <input type="text" name=""/>
+          <p>Nome:<input type="text" name="nome"/></p>
         </div>
 
         <div style="width: 50%"> 
-          <input type="email" name="email" id="email" placeholder="e-mail"> 
+          <p>Email:<input type="email" name="email" id="email" placeholder="e-mail"> </p>
+        </div>
+        
+         <div style="width: 50%">
+          <p>Empresa de atuação:<input type="text" name="empresa"/></p>
         </div>
 
 <label title="ooo"/></label>
-        <div class="radioButtonOptions" value=""> 
+        <div class="radioButtonOptions" value="">
+         <p>Você sabe o que é Varejo 2.0?</p> 
           <input type="radio" name="tipoComercio" value="Sim"> 
           <label for="varejosim">Sim</label> 
           <input type="radio" name="tipoComercio" value="Nao"> 
           <label for="varejonao">Não</label> 
         </div> 
+<label title="concorrencia"/></label>    
+         <div class="radioButtonOptions" value="">
+         <p>Para você, qual o grau de importância do varejo 2.0 para análise de concorrência? </p> 
+          <input type="radio" name="importancia1" value="1"> 
+          <label for="1">1</label> 
+          <input type="radio" name="importancia2" value="2"> 
+          <label for="2">2</label> 
+          <input type="radio" name="importancia3" value="3"> 
+          <label for="3">3</label> 
+          <input type="radio" name="importancia4" value="4"> 
+          <label for="4">4</label> 
+          <input type="radio" name="importancia5" value="5"> 
+          <label for="5">5</label> 
+        </div> 
+        
+  <label title="informacao"/></label>    
+         <div class="radioButtonOptions" value="">
+         <p>Você acha que a rapidez na obtenção da informação é importante? </p> 
+          <input type="radio" name="informacao1" value="1"> 
+          <label for="1">1</label> 
+          <input type="radio" name="informacao2" value="2"> 
+          <label for="2">2</label> 
+          <input type="radio" name="informacao3" value="3"> 
+          <label for="3">3</label> 
+          <input type="radio" name="informacao4" value="4"> 
+          <label for="4">4</label> 
+          <input type="radio" name="informacao5" value="5"> 
+          <label for="5">5</label> 
+        </div> 
+        
+        	<div>
+        	Para você, qual a relação dele com o giro de estoque do produto?<br>
+        	<input type="checkbox" name="opcao1" value="0"> Impacta em como eu enxergo meu estoque e as estratégias relacionadas a ele<br>
+        	<input type="checkbox" name="opcao2" value="1"> Aumenta meu controle sobre o ciclo de vida do produto<br>
+        	<input type="checkbox" name="opcao3" value="2"> Não tem nada a ver com giro de estoque
+        	  
+	 		 </div>
 
       </div>
 

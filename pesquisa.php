63841dca162dff90bf1557a8e490ae827a50d2e2
JC Bombardelli <jc.bombardelli@live.com>
2017-03-21 00:42:15 -0300

    pesquisa [Added]

---
+++ pesquisa.php
@@ -0,0 +1,46 @@
+<?php require(__DIR__."/components/constants.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title><?=WEBSITE_TITLE?>Rua Varejo</title>
+    <?php require(__DIR__."/components/import.html") ?>
+    <link rel="stylesheet" type="text/css" href="/css/posts.css">
+  </head>
+
+  <body>
+    <?php require(__DIR__."/components/navigator.html"); ?>
+    <?php require(__DIR__."/components/page_header.html");?>
+    <span class="post">
+      <!--Posts e divulgação de ebook-->
+      <div class="col-md-9" style="padding-top: 5%; padding-right:10%; padding-left: 10%; padding-bottom: 5%">
+        <h1>PESQUISA VAREJO 2.0</h1>
+        <h2 style="color:#808080">Esta pesquisa busca entender qual o grau de importância do Varejo 2.0 e como ele está relacionado às diferentes etapas no processo de vendas.</h2>
+      <div class="clearfix colelem" id="u12639-32"><!-- content -->
+
+        <div style="width: 50%">
+          <input type="text" name=""/>
+        </div>
+
+        <div style="width: 50%"> 
+          <input type="email" name="email" id="email" placeholder="e-mail"> 
+        </div>
+
+<label title="ooo"/></label>
+        <div class="radioButtonOptions" value=""> 
+          <input type="radio" name="tipoComercio" value="Sim"> 
+          <label for="varejosim">Sim</label> 
+          <input type="radio" name="tipoComercio" value="Nao"> 
+          <label for="varejonao">Não</label> 
+        </div> 
+
+      </div>
+
+      </div>
+
+    </span>
+  </body>
+</html>/

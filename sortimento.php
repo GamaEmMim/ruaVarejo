b5693d0cfacb1bffea990ca14835b512843a3659
Hiagor Queiros de Oliveira <hiagornew@gmail.com>
2017-03-21 22:17:19 -0300

    Pagina Sortimento

---
+++ sortimento.php
@@ -0,0 +1,90 @@
+<?php require(__DIR__."/components/constants.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title><?=WEBSITE_TITLE?>Rua Varejo</title>
+    <?php require(__DIR__."/components/import.html") ?>
+    <link rel="stylesheet" type="text/css" href="/css/home.css">
+  </head>
+
+  <body>
+    <?php require(__DIR__."/components/navigator.html"); ?>
+    <?php require(__DIR__."/components/page_header.html");?>
+
+      <!--Posts e divulgação de ebook-->
+      <div class="col-md-9" style="padding-top: 5%; padding-right:10%; padding-left: 10%; padding-bottom: 5%">
+       <span class="post">
+        <h1 style="color:#FF0000">Omnichannel e Sortimento: como conciliar esses 2 desafios?</h1>
+        <h2 style="color:#808080">Se fazer o sortimento de um canal já era difícil...</h2>
+        
+
+      <div class="clearfix colelem" id="u12639-32"><!-- content -->
+        <h3><p align="justify">Nos últimos anos o processo de planejamento no varejo cresceu e se tornou ainda
+
+mais complexo. Planejadores de sortimento precisam considerar marcas, produtos,
+
+cores e tamanhos preferidos por consumidores; ao mesmo tempo eles precisam
+
+controlar as vendas online e nas lojas físicas. Essas complexidades foram ampliadas
+
+enquanto os varejistas crescem online e abrem novas lojas.</p></h3>
+        <h6>&nbsp;</h6>
+
+        <h3><p align="justify">Como visto em nosso ebook sobre Omnichannel, o conceito coloca o consumidor
+
+no centro das ações da marca, em todos os canais, onde ele estiver, seja no digital
+
+(em redes sociais, site, mobile, etc.) como no físico (lojas, stands e eventos), tudo
+
+de forma uniforme e constante.</p></h3>
+        <h6>&nbsp;</h6>
+
+        <h3><p align="justify">E aí surge um novo desafio: como conciliar o sortimento de forma que o consumidor encontre os produtos que quer em todos os canais da mesma marca/empresa?</p></h3>
+        <h6>&nbsp;</h6>
+
+        <h3 style="color:#FF0000"><p align="justify">Veja os maiores desafios, conhecer um case genial e ainda um roteiro para
+
+montar um planejamento perfeito no nosso E-book!</p></h3>
+        
+
+
+       <h2><p align="justify">Para mais dicas sobre o universo do varejo acesse nosso  
+       <a href="https://www.linkedin.com/company/16219656" target="_blank"> Linkedin </a>,  
+       <a href="http://www.facebook.com/ruavarejo" target="_blank"> Facebook </a> e verifique nossas outros posts...</p></h2>
+       <h6>&nbsp;</h6>
+       
+       </span>
+      </div>
+
+        <hr class="divisor">
+
+        <div class="row">
+          <div class="col-md-5">
+           <a class="signUpModal" href="#">
+            <img class="img-rounded" style="width: 100%; height: 300px" src="/images/ebook3.png"></a>
+          </div>
+          <div class="col-md-5">
+           <a class="signUpModal" href="#">
+            <img class="img-rounded" style="width: 100%; height: 300px" src="/images/ebook1.png">
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <!--Banner Lateral-->
+      <div class="col-md-2">
+        <div class="row">
+         <a class="signUpModal" href="#">
+          <img src="/images/banner.jpg">
+          </a>
+        </div>
+      </div>
+    </div>
+  </body>
+
+
+</html>
